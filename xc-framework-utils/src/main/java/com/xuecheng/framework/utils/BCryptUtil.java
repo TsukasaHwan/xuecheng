65140b9525619a2a8786a79f46558dcfc3390554
admin <785415580@qq.com>
2019-03-13 20:07:15 +0800

    提交学成在线代码

---
+++ xc-framework-utils/src/main/java/com/xuecheng/framework/utils/BCryptUtil.java
@@ -0,0 +1,18 @@
+package com.xuecheng.framework.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by mrt on 2018/5/22.
+ */
+public class BCryptUtil {
+    public static String encode(String password){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
+    }
+    public static boolean matches(String password,String hashPass){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(password, hashPass);
+    }
+}
---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng.manage_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages = "com.xuecheng.api")//扫描api接口
+@ComponentScan(basePackages = "com.xuecheng.manage_cms")//扫描本类所在的包及以下
+public class ManageCmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsApplication.class, args);
+    }
+}
